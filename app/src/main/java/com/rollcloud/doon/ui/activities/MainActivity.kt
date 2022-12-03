e9704ae4570c47f6a9ff3f71c7eb637d527e48af
rollcloud <rollcloud@gmail.com>
2022-12-03 17:20:34 +0000

    fix: simplify calculation of total score

--- app/src/main/java/com/rollcloud/doon/ui/activities/MainActivity.kt
+++ app/src/main/java/com/rollcloud/doon/ui/activities/MainActivity.kt
@@ -22,7 +22,6 @@ import com.rollcloud.doon.ui.adapters.TaskAdapter
 import java.lang.System.currentTimeMillis
 import kotlin.math.absoluteValue
 import kotlin.math.min
-import kotlin.math.roundToInt
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -145,12 +144,8 @@ class MainActivity : AppCompatActivity() {
 
   fun updateScore() {
     val now: Instant = clock.now()
-    val taskScores =
-      actionsTasks.map {
-        min(it.getDaysTillDue(), 0) - min((it.movingAverageFrequency(1) ?: 0F).roundToInt(), 0)
-      }
+    val taskScores = actionsTasks.map { min(it.getDaysTillDue(), 0) }
     val totalScore = taskScores.sum()
-    //    showScore.text = totalScore.toHoursAndDays(signed = true)
     showScore.text = "${totalScore.absoluteValue} task⋅days"
     showScore.setTextColor(
       resources.getColor(
